01703ad80c2160a84d30bad5c3645e5f0820302e
wwm0609 <wwm0609@gmail.com>
2019-01-30 10:13:18 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo 'updating apt cache'
+apt update
+
 # install
 npm install hexo --save
 
@@ -10,5 +13,6 @@ npm install hexo-asset-image --save
 # 安装gittalk作为评论系统
 npm i --save gitalk
 
-
 npm install hexo-deployer-git --save
+
+echo "all set, you can run 'hexo g && hexo s' to preview your pages"
